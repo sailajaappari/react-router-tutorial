509fc926b2a32a4a52ca677efaf8f1527a4a5808
sailajaappari <sailajakadali@gmail.com>
2016-06-07 11:28:24 +0530

    3rd lesson

---
+++ lessons/03-navigating-with-link/modules/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <div>About</div>
+        <div>This is About file</div>
+      </div>
+    )
+   
+  }
+})
